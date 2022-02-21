659fc25e24673fb8ad3db4887eba762a652fddd6
hayk <haykk2018@gmail.com>
2022-02-21 19:09:01 +0400

    Initial commit

---
+++ src/DotCom.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class DotCom {
+    private ArrayList<String> locationCells;
+    private String name;
+
+    public void setLocationCells(ArrayList<String> locs) {
+        locationCells = locs;
+    }
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String checkYourself(String userInput) {
+        //int guess = Integer.parseInt(stringGuess);
+        int index = locationCells.indexOf(userInput);
+        String result = "Мимо";
+        if (index >= 0) {
+            locationCells.remove(index);
+            if (locationCells.isEmpty()) {
+                result = "Потопил";
+                System.out.println("Ой! Вы потопили " + name);
+            } else {
+                result = "Попал";
+            }
+        }
+        return result;
+
+    }
+}
